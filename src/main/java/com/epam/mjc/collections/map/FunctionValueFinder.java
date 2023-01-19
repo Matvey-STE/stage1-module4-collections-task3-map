0380128b392128de64b427a91f3d00b0150689c7
Matvey Stetcenko <matveyvs1987@gmail.com>
2023-01-20 03:44:49 +0400

    Problem solved

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,26 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+        boolean result = false;
+        for(Map.Entry<Integer, Integer> value : functionMap.entrySet()){
+            if (value.getValue() == requiredValue){
+                result = true;
+            }
+        }
+        return result;
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer element: sourceList){
+            map.put(element,(5*element+2));
+        }
+        return map;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,21 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String,Integer> map = new HashMap<>();
+        String result = sentence.toLowerCase()
+                .replaceAll(",", "")
+                .replaceAll("\\.", "");
+        String[] words = result.split(" ");
+        if(sentence.isEmpty()){
+            return map;
+        } else {
+        for (String word : words){
+            map.merge(word, 1, Integer::sum);
+        }}
+        return map;
     }
 }
